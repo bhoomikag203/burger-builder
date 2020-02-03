24f74f82035718653bda589ffc73be554a1bde9a
bhoomikag203 <bhoomikag203@gmail.com>
2020-02-03 14:55:00 +0530

    implementing addingredient handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    meat: 0.8,
+    cheese: 0.5,
+    salad: 0.4,
+    bacon: 0.7,
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,14 +17,32 @@ class BurgerBuilder extends Component {
             cheese: 0,
             salad: 0,
             bacon: 0,
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+
+    }
+
+
     render() {
         return (
             <Aux>
                 <div>Burger</div>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </Aux>
         )
     }
